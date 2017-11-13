b8760cd926422adc3a9de8afb32cc8f059e86bf7
NivedRajaraman <nived.rajaraman@gmail.com>
2017-11-14 03:15:56 +0530

    update files

--- FFAST.m
+++ FFAST.m
@@ -1,46 +1,62 @@
 
-function [DFT] = FFAST (FFTarray, N1, N2, d, N1sampleArray, N2sampleArray)
+function [DFT] = FFAST (FFTarray, N1, N2, d, sampleArray)
 
 % N1, N2 -> dimensions of input array
 % d -> number of delays
-% N1sampleArray, N2sampleArray :: same length
+% sampleArray -> array with the number of times to subsample
 
     array = ifft2(FFTarray);
 
     dim = N1*N2;
     K = zeros(N1,N2);
-    N = zeros(N1,N2);    
+    N = zeros(N1,N2);
+    
+    for n = (0 : N1-1);
+        for m = (0 : N2-1);
+            K(n+1,m+1) = mod(N2*n + N1*m, dim);
+        end
+    end
+    K = reshape(K,1,N1*N2);
+    
+    for k = (0 : N1-1)
+        for l = (0 : N2-1)
+            N(k+1,l+1) = mod(k*invMod(N2, N1)*N2 + l*invMod(N1, N2)*N1, dim);
+        end
+    end
+    N = reshape(N,1,N1*N2);
+    
+    % q = [0, 16, 12, 8, 4; 5, 1, 17, 13, 9; 10, 6, 2, 18, 14; 15, 11, 7, 3, 19];
+    
     p = (1:N1*N2);
+    CRTArray(p) = array(N(p)+1);
     
     % cellArray = cell(length(sampleArray));
-    for i = (1 : length(N1sampleArray))
-        sample00 = downsample(downsample(array(:,:), N1sampleArray(i))', N2sampleArray(i))';
+    
+    for i = (1 : length(sampleArray))
+        SubsampledArray = downsample(CRTArray(p), sampleArray(i));
         if d > 1
             for j = (1 : d-1)
-                sample10 = downsample(downsample(array(:,:), N1sampleArray(i), 1)', N2sampleArray(i), 0)';
-                sample01 = downsample(downsample(array(:,:), N1sampleArray(i), 0)', N2sampleArray(i), 1)';
-
-                SubsampledArray = [ sample00 ; sample10 ; sample01 ];
+                sample = downsample(CRTArray(p), sampleArray(i), j);
+                SubsampledArray = [ SubsampledArray ; sample ];
             end
         end
-        SubsampledArrayFFT = N2sampleArray(i)*N1sampleArray(i)*[ fft2(sample00) ; fft2(sample10) ; fft2(sample01) ];
+        c = SubsampledArray.';
+        SubsampledArrayFFT = sampleArray(i)*fft(c).';
         subsampledCell{i} = SubsampledArray;
         FFTsubsampledCell{i} = SubsampledArrayFFT;
-        N1stride = N1/N1sampleArray(i);
-        N2stride = N2/N2sampleArray(i);
-
-        for j = (1:N1stride)
-            for k = (1:N2stride) % indexing with stream no. also
-                Yijk = [ SubsampledArrayFFT(j,k) ; SubsampledArrayFFT(j+N1stride,k) ; SubsampledArrayFFT(j+2*N1stride,k) ];
-                Y{j,k} = Yijk;
-            end
-        end
-        YStream{i} = Y;
-        Y = [];
     end
     % cellArray now holds in each cell a 2d array of subsampled and delayed
     % inputs
     
-    DFT = PeelingSolver(YStream, N1, N2, N1sampleArray, N2sampleArray);
+    DFT = PeelingSolver(FFTsubsampledCell, d, sampleArray, dim);
+    %DFT = reshape(DFT, N2, N1)';
+    finalDFT = zeros(N1, N2);
+
+    [G, U, V] = gcd(N1, N2);    % G = 1, U & V solve Diophatine Equations
+    for i = (0:dim-1)
+        n1 = mod(V*i, N1);
+        n2 = mod(U*i, N2);
+        finalDFT(n1+1, n2+1) = DFT(i+1);
+    end
     a = 1;
 end
--- main.m
+++ main.m
@@ -1,18 +1,15 @@
 
-% Input Sparse DFT array with:
-% k = 3
-% N1 = 336 = 16*21
-% N2 = 323 = 17*19
-% Rate of subsampling: 16 (vertically) * 17 (horizontally) -> /272
-%                      21 (vertically) * 19 (horizontally) -> /399
+ary = zeros(4,5);
+ary(1,2) = 1;
+ary(1,4) = 4;
+ary(2,1) = 1;
+ary(3,1) = 3;
+ary(3,4) = 7;
 
-inputArray = zeros(304,306);
-inputArray(3,3) = 10;
-inputArray(233,100) = 23;
-inputArray(145,301) = 1;
-inputArray(33,166) = 1.5;
+q = [0, 16, 12, 8, 4; 5, 1, 17, 13, 9; 10, 6, 2, 18, 14; 15, 11, 7, 3, 19];
 
-% Performing DFT on the iDFT of the sparse array. (Implicit iDFT performed)
-DFT = FFAST(inputArray,304,306,2,[16,19],[18,17]);
+ar = zeros(4,5);
+ar(2,2) = 1;
+ar(3,4) = 1;
 
-working = any(any((inputArray - DFT)));
+FFAST(ar,4,5,3,[4,5]);
